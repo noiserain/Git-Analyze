94ee8d2853686b111028d301a42248732859124d
noiserain <kimhan0913@naver.com>
2026-06-04 14:22:35 +0900

    검색창 제안 기능 추가

--- src/components/BookmarkedUsers.tsx
+++ src/components/BookmarkedUsers.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { GitHubUser } from '../types';
 import { Bookmark, UserX, GripVertical } from 'lucide-react';
 
